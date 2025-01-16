ae5e311ead8576426ce7b6f664d86d0dfed9c83c
wilson <wilson8548125@gmail.com>
2025-01-16 06:37:40 -0400

    segundo commit contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <p>{{ contador }}</p>
+      <button @click="incrementar">Incrementar</button>
+      <button @click="decrementar">Decrementar</button>
+    </div>
+</template>
+  
+  <script setup>
+    import { ref } from 'vue';
+  
+    const contador = ref(0);
+  
+    const incrementar = () => {
+      contador.value++;
+    }
+    const decrementar = () => {
+      contador.value--;
+    }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
