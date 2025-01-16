4e75eb0b10fddbaa4bc0d5b800e76d6beef62950
wilson <wilson8548125@gmail.com>
2025-01-16 07:09:29 -0400

    commit 4 router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <Contador></Contador>
-  <ListaDeTareas></ListaDeTareas>
+  <MainView></MainView>
 </template>
 
 <script setup>
-  import Contador from './components/Contador.vue';
-  import ListaDeTareas from './components/ListaDeTareas.vue';
+  import MainView from './views/MainView.vue'; 
+
 </script>
 
 <style scoped>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li>
+                <router-link to="/contador">Contador</router-link>
+            </li>
+            <li>
+                <router-link to="/lista-de-tareas">Lista de Tareas</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    .sidebar{
+       width: 200px;
+       background-color: #f0f0f0;
+       padding: 20px; 
+    }
+    li{
+        text-decoration: none;
+        list-style: none;
+    }
+</style>
