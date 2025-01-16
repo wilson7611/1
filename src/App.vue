9d72c36fe2cf7e01dd254aca326a1867710291d7
wilson <wilson8548125@gmail.com>
2025-01-16 06:55:41 -0400

    commit 3 lista de tareas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Contador></Contador>
+  <ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
   import Contador from './components/Contador.vue';
+  import ListaDeTareas from './components/ListaDeTareas.vue';
 </script>
 
 <style scoped>
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+    </div>
+    <div>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+    </div>
+    <div>
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea">Eliminar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+    import {ref} from 'vue';
+
+    const tareas = ref([]);
+    const nuevaTarea = ref('');
+
+    const addTarea = () => {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = '';
+    }
+    const borrarTarea = (index) => {
+        tareas.value.splice(index, 1);
+    }
+</script>
+<style></style>
