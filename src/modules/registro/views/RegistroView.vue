d1be87d5717d367523653376a1e500c56c7b30a6
wilson <wilson8548125@gmail.com>
2025-01-16 07:37:10 -0400

    formulario con validacion

---
+++ src/modules/registro/views/RegistroView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Formulario de registro</h2>
+        <Form :validation-schema="schema" @submit="onSubmit">
+            <div class="form">
+                <label for="nombre">Nombre: </label>
+                <Field type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" />
+                <ErrorMessage name="nombre"></ErrorMessage>
+            </div>
+            <div class="form">
+                <label for="email">Correo: </label>
+                <Field type="text" name="email" id="nombre" placeholder="Ingrese su correo" />
+                <ErrorMessage name="email"></ErrorMessage>
+            </div>
+            <div class="form">
+                <button type="submit">Registrar</button>
+            </div>
+        </Form>
+    </div>
+</template>
+
+<script setup>
+    import { Form, Field, ErrorMessage } from 'vee-validate';
+    import {schema} from '../schemas/ValidationSchema';
+
+    const onSubmit = () => {
+        console.log("se ha Enviado el registro de formulario");
+        
+    }
+</script>
+
+<style scoped>
+    .form {
+        margin-bottom: 10px;
+    }
+</style>
